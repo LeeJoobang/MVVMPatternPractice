d24f1cd2766a4e3beaa7851bc57e0baf7f000435
LeeJoobang <hii5074@gmail.com>
2023-01-27 12:02:28 +0900

    [add] tableviewCell

---
+++ GoodNews/GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by 사용자 on 2023/01/27.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
