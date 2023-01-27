36d337a140568d917865e5dd596bc114e2c5072e
LeeJoobang <hii5074@gmail.com>
2023-01-27 11:45:04 +0900

    [add] ArticleViewModel 추가

---
+++ GoodNews/GoodNews/View Models/ArticleViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ArticleViewModel.swift
+//  GoodNews
+//
+//  Created by 사용자 on 2023/01/27.
+//
+
+import Foundation
+
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+extension ArticleListViewModel{
+    
+    var numberOfSection: Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel{
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+}
+
+struct ArticleViewModel {
+    private let article: Article
+}
+
+extension ArticleViewModel {
+    init(_ article: Article){
+        self.article = article
+    }
+}
+
+extension ArticleViewModel {
+    var title: String {
+        return self.article.title
+    }
+    
+    var description: String {
+        return self.article.description
+    }
+}
