f2dee6df32a002d4e2ec5c33ce7ee57eb270f296
LeeJoobang <hii5074@gmail.com>
2023-01-27 10:05:21 +0900

    [add] getArticle

---
+++ GoodNews/GoodNews/Services/WebService.swift
@@ -0,0 +1,23 @@
+//
+//  WebServices.swift
+//  GoodNews
+//
+//  Created by 사용자 on 2023/01/27.
+//
+
+import Foundation
+
+class WebService {
+    
+    func getArticle(url: URL, completion: @escaping ([Any]?) -> ()){
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                print(data)
+            }
+        }
+    }
+}
--- GoodNews/GoodNews/Services/WebServices.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WebServices.swift
-//  GoodNews
-//
-//  Created by 사용자 on 2023/01/27.
-//
-
-import Foundation
